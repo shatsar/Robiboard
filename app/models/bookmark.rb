b011de98bb972428c7c3b772b248085eb27e5eb3
Michele Zonca <shatsar@gmail.com>
2013-01-12 19:24:24 -0800

    adding snapshot to bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ActiveRecord::Base
   attr_accessible :description, :image_url, :title, :category_id
+  has_attached_file :snapshot, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
   belongs_to :category
 end

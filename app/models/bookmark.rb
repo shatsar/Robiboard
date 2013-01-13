0ccb0129f1718ed0a24255cc506772d0253b2433
Michele Zonca <shatsar@gmail.com>
2013-01-12 17:58:33 -0800

    setting bookmark category

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,4 @@
 class Bookmark < ActiveRecord::Base
-  attr_accessible :description, :image_url, :title
+  attr_accessible :description, :image_url, :title, :category_id
   belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112024942) do
+ActiveRecord::Schema.define(:version => 20130113015132) do
 
   create_table "bookmarks", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130112024942) do
     t.string   "image_url"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
   create_table "categories", :force => true do |t|

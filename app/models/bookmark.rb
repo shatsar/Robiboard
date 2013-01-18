f92b1ff907fecac5dbcb134e0637fb8adf2ec93a
Michele Zonca <shatsar@gmail.com>
2013-01-17 17:06:21 -0800

    missing image placeholder

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ActiveRecord::Base
   attr_accessible :description, :url, :title, :category_id, :snapshot
-  has_attached_file :snapshot, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :snapshot, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/missing.png"
   belongs_to :category
   self.per_page = 3
   

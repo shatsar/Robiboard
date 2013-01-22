13b558bee3d134fa0ae84f27a6e476dd8eb7bbd8
Michele Zonca <shatsar@gmail.com>
2013-01-22 13:45:07 -0800

    starting first search implementation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
      query = params[:query]
+	 @categories = Category.find(:all, :conditions => ["title LIKE :query", {:query => query}])
   end
 end

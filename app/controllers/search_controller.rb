405513516708b12076a603719cb8bd11bb9898c6
Michele Zonca <shatsar@gmail.com>
2013-01-22 14:09:03 -0800

    search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
-     query = params[:query]
-	 @categories = Category.find(:all, :conditions => ["title LIKE :query", {:query => query}])
+     @query = params[:query]
+	 @categories = Category.find(:all, :conditions => ["lower(title) LIKE :query", {:query => "%#{@query.downcase}%"}])
+	 @bookmarks = Bookmark.find(:all, :conditions => ["lower(description) LIKE :query", {:query => "%#{@query.downcase}%"}])
   end
 end

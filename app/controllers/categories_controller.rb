8b71fafd84dc68a60df096c94f210464ce66526b
Michele Zonca <shatsar@gmail.com>
2013-02-13 12:18:32 -0800

    option to add a category from the category view with parent_id populated

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,6 +25,7 @@ class CategoriesController < ApplicationController
   # GET /categories/new.json
   def new
     @category = Category.new
+	@category.parent_id = params[:parent_id]
 
     respond_to do |format|
       format.html # new.html.erb

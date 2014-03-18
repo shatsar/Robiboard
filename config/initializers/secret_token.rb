55192ba86da8149b6e8f12a5843502acb73928eb
shatsar <shatsar@gmail.com>
2014-03-18 15:57:33 +0100

    starting upgrade: rails 4 and bootstrap 3

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Robiboard::Application.config.secret_token = '1cbbf31fdbd54b2cb6f05858ecb27c33efa552e7c4e141275f56016aeacf11a083b652480b62125e1621f4e9281ef3ea92d5ac24224e3c078fa90ddf88dd5c89'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Robiboard::Application.config.secret_key_base = 'daf75f107aae9d3921d58395ef71095d32335683f89b0c1f3d3ddeb1fa420bba95ba0952fcf2c9de06492225bf779a33d0ddf3c485f8a9659018310fe939219d'

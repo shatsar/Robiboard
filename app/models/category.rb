a32fb65f99438f3e0e5b7682536a6f0d114234e2
shatsar <shatsar@gmail.com>
2014-04-17 11:28:08 +0200

    add default sort order to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ActiveRecord::Base
   extend FriendlyId
   attr_accessible :title, :parent_id
   acts_as_nested_set
+  default_scope order('title ASC')
   
   has_many :bookmarks 
   friendly_id :title, use: :slugged

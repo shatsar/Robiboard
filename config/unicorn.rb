9486bd7e8df62c21c9bda8ecc478d8cd0772a2f0
shatsar <shatsar@gmail.com>
2014-04-18 10:53:46 +0200

    socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,7 +25,7 @@ working_directory "/home/michele/Robiboard/" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/home/michele/Robiboard.unicorn.sock", :backlog => 64
+listen "/tmp/Robiboard.unicorn.sock", :backlog => 64
 listen 3000, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
